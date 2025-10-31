a7ccb2b2f507298ce6a799af6ea0b54315e8aad3
osarsar <osarsarjob@gmail.com>
2025-10-31 17:29:58 +0100

    v3

--- react/vite.config.js
+++ react/vite.config.js
@@ -1,15 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // 👉 ton code est sous react/frontend
-  root: 'frontend',
-  // 👉 le build sortira dans react/dist
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
-  // 👉 correspond EXACTEMENT au nom du repo GitHub
-  base: '/Mzlearninghub/',
+  // très important pour un "project page" GH Pages
+  base: '/',
 })
